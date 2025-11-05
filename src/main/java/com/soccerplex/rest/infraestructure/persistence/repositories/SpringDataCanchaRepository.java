200ad86f794a34cf9ee504a5177f21bdd282c4db
NeoArsenal <149034867+NeoArsenal@users.noreply.github.com>
2025-11-05 15:08:33 -0500

    Update SpringDataCanchaRepository.java

--- src/main/java/com/soccerplex/rest/infraestructure/persistence/repositories/SpringDataCanchaRepository.java
+++ src/main/java/com/soccerplex/rest/infraestructure/persistence/repositories/SpringDataCanchaRepository.java
@@ -3,7 +3,12 @@ package com.soccerplex.rest.infraestructure.persistence.repositories;
 import com.soccerplex.rest.infraestructure.persistence.entities.CanchaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Patrón aplicado: Repository
+ * Rol: Abstracción de la capa de acceso a datos.
+ * Justificación: Permite interactuar con la base de datos sin exponer detalles
+ * de persistencia al dominio o a los servicios.
+ */
 @Repository
 public interface SpringDataCanchaRepository extends JpaRepository<CanchaEntity, Long> {
     // Spring ya nos da: save, findById, deleteById, existsById, findAll
