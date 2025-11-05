4620c4c49e63cf1e04b1c0edf2eb09279c570a1e
NeoArsenal <149034867+NeoArsenal@users.noreply.github.com>
2025-11-05 15:03:51 -0500

    Update Usuario.java

--- src/main/java/com/soccerplex/rest/domain/model/Usuario.java
+++ src/main/java/com/soccerplex/rest/domain/model/Usuario.java
@@ -1,7 +1,12 @@
 package com.soccerplex.rest.domain.model;
 
 import lombok.Data;
-
+/**
+ * Patrón aplicado: Entity (DDD)
+ * Rol: Representa un objeto del dominio con identidad propia.
+ * Justificación: Modela los objetos del negocio (Cancha, Cliente, Reserva)
+ * y encapsula el estado y comportamiento del dominio.
+ */
 @Data
 public class Usuario {
 
